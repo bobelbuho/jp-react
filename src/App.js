5688bbcaf24ac405dd9dd6deec47867610b75f8c
Bob <bob.vaz@hotmail.fr>
2023-10-21 16:14:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import NavBar from "./compenents/NavBar";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
